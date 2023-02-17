f16b36a322b74402c92bd32a4c7fbcc71c5a5fce
Juan Pablo Valencia <pibe742337@gmail.com>
2023-02-16 20:38:07 -0500

    Segundo video mitad

---
+++ model/dto/period.dto.js
@@ -0,0 +1,24 @@
+/** packages */
+const mongoose = require("mongoose");
+
+/** using schemas */
+const schema = require("../schemas/period.schema");
+
+schema.statics = {
+    create: function (data, cb){
+        let doc = new this(data);
+        doc.save(cb);
+    },
+    getAll: function (query, cb){
+        doc.find(query, cb);
+    },
+    getByCode: function (query, cb){
+        doc.find(query, cb);
+    },
+    update: function (query, data, cb){
+        this.findOneAndUpdate(query, {$set: data}, {new: true}, cb);
+    },
+    delete: function(query, cb){
+        this.findOneAndDelete(query);
+    }
+};
